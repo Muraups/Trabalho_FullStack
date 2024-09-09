333477c9df43c56338ed7e4c99a3c14d7d0560f8
Muraups <muriloaugusto.p9@edu.unifil.br>
2024-09-09 00:33:02 -0300

    ajustes

--- Back/Models/Pessoa.js
+++ Back/Models/Pessoa.js
@@ -2,17 +2,20 @@
 
 const {Model} = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-    class Pessoa extends Model {
-        static associate(Models) {
-        }
-    }
-    Pessoa.init({
-        noem: DataTypes.STRING,
-        CPF: DataTypes.STRING,
-        Telefone: DataTypes.STRING,
-    }, {
-        sequelize,
-        modelName: 'Pessoa',
-    });
+    const Pessoa = sequelize.define('Pessoa', {
+      nome: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      cpf: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      telefone: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    }, {});
     return Pessoa;
-}
+  };
